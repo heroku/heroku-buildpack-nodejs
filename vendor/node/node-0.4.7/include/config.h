f8d70440a9a7e9040a9c28c8807671c1cb714614
David Dollar <ddollar@gmail.com>
2011-06-02 12:35:15 -0400

    restructure node vendoring, add includes and waf

---
+++ vendor/node/node-0.4.7/include/config.h
@@ -0,0 +1,29 @@
+/* Configuration header created by Waf - do not edit */
+#ifndef _CONFIG_H_WAF
+#define _CONFIG_H_WAF
+
+#define HAVE_OPENSSL 1
+#define HAVE_PTHREAD_CREATE 1
+#define HAVE_PTHREAD_ATFORK 1
+#define HAVE_FUTIMES 1
+/* #undef HAVE_READAHEAD */
+#define HAVE_FDATASYNC 1
+#define HAVE_PREADWRITE 1
+#define HAVE_SENDFILE 1
+/* #undef HAVE_SYNC_FILE_RANGE */
+/* #undef HAVE_SYS_INOTIFY_H */
+/* #undef HAVE_SYS_EPOLL_H */
+/* #undef HAVE_PORT_H */
+#define HAVE_POLL_H 1
+#define HAVE_POLL 1
+#define HAVE_SYS_EVENT_H 1
+#define HAVE_SYS_QUEUE_H 1
+#define HAVE_KQUEUE 1
+#define HAVE_SYS_SELECT_H 1
+#define HAVE_SELECT 1
+/* #undef HAVE_SYS_EVENTFD_H */
+/* #undef HAVE_CLOCK_SYSCALL */
+#define HAVE_NANOSLEEP 1
+#define HAVE_CEIL 1
+#define HAVE_CONFIG_H 1
+#endif /* _CONFIG_H_WAF */
