12020b35691fd91ecae09cb20b3a7d11b2654119
zeke <zeke@sikelianos.com>
2013-10-09 23:26:24 -0700

    use semver.io for great success

--- bin/common.sh
+++ bin/common.sh
@@ -1,45 +1,3 @@
-#!/usr/bin/env bash
-
-# fail fast
-set -e
-
-# Uncomment the line below to enable debugging
-# set -x
-
-download_and_install_node() {
-  version="$1"
-  node_url="http://s3pository.heroku.com/node/v$version/node-v$version-linux-x64.tar.gz"
-  curl $node_url -s -o - | tar xzf - -C $build_dir
-  mkdir -p $build_dir/vendor
-
-  # Remove node in case we're overwriting a previously-downloaded version
-  rm -rf $build_dir/vendor/node
-
-  mv $build_dir/node-v$version-linux-x64 $build_dir/vendor/node
-  chmod +x $build_dir/vendor/node/bin/*
-  PATH=$PATH:$build_dir/vendor/node/bin
-}
-
-query_stable_version() {
-  curl -s http://nodejs.org/dist/ \
-    | egrep -o '[0-9]+\.[0-9]*[02468]\.[0-9]+' \
-    | sort -u -k 1,1n -k 2,2n -k 3,3n -t . \
-    | tail -n1
-}
-
-query_latest_version() {
-  curl -s http://nodejs.org/dist/ \
-    | egrep -o '[0-9]+\.[0-9]+\.[0-9]+' \
-    | sort -u -k 1,1n -k 2,2n -k 3,3n -t . \
-    | tail -n1
-}
-
-query_all_versions() {
-  curl -s http://nodejs.org/dist/ \
-  | egrep -o '[0-9]+\.[0-9]+\.[0-9]+' \
-  | sort -u -k 1,1n -k 2,2n -k 3,3n -t .
-}
-
 error() {
   echo " !     $*" >&2
   exit 1
@@ -47,6 +5,13 @@ error() {
 
 status() {
   echo "-----> $*"
+}
+
+protip() {
+  echo
+  echo "PRO TIP: $*" | indent
+  echo "See https://devcenter.heroku.com/articles/nodejs-support"
+  echo
 }
 
 # sed -l basically makes sed replace and buffer through stdin to stdout
@@ -60,6 +25,6 @@ indent() {
   esac
 }
 
-function cat_npm_debug_log() {
+cat_npm_debug_log() {
   test -f $build_dir/npm-debug.log && cat $build_dir/npm-debug.log
 }
