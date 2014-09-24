f90f4590f52be51dec115bd2add5faefd8f89936
Soulou <leo@unbekandt.eu>
2014-09-24 11:56:05 +0200

    Support of meteor (tested with 0.8 and 0.9.2.2-latest)

--- bin/meteor.sh
+++ bin/meteor.sh
@@ -22,27 +22,33 @@ demeteorize_app() {
 
   meteor_temp=$(mktempdir meteor)
   METEOR_HOME=$(pwd)/$meteor_temp
-  curl -Ls https://install.meteor.com | HOME=$METEOR_HOME PREFIX=$build_dir/vendor/node /bin/sh | indent
+  curl -Ls https://install.meteor.com | HOME=$METEOR_HOME PREFIX=$build_dir/.vendor/node /bin/sh | indent
   status "Meteor installed"
 
-  npm  install -g meteorite demeteorizer | indent
+  if [ -e "$build_dir/smart.json" ] ; then
+    if [ ! -e "$build_dir/smart.lock" ] ; then
+      error "smart.lock is not present, run 'mrt install' to freeze dependencies"
+    fi
+    npm install -g meteorite | indent
+    status "Meteorite installed"
+    mrt install | indent
+    status "Meteorite packaged installed"
+  fi
+
+  npm  install -g demeteorizer | indent
   status "Demeteorizer installed"
 
-  mkdir meteor_app
-  mv .meteor meteor_app
+  HOME=$METEOR_HOME PREFIX=$build_dir/.vendor/node demeteorizer -o "$build_dir/demeteorized" | indent
 
-  for file in * .* ; do
-    if echo "$file" | grep -qvE "^\\.|\\.\\.|meteor_app|tmp|vendor|${meteor_temp}|Procfile|\\.npm$" ; then
-      mv "$file" meteor_app
-    fi
-  done
+  if [ ! -e "$build_dir/Procfile" ] ; then
+    echo "web: cd demeteorized && npm start" > "$build_dir/Procfile"
+  fi
 
-  cd meteor_app
-  HOME=$METEOR_HOME PREFIX=$build_dir/vendor/node demeteorizer -o "$build_dir/demeteorized" -n "v$node_version" | indent
-  cd $build_dir
-
-  ln -s "$build_dir"/demeteorized/package.json "$build_dir"/package.json
-  sed -i 's/node main\.js/node demeteorized\/main.js/' "$build_dir"/package.json
+  if [ ! -d "$build_dir/node_modules" ] ; then
+    mkdir "$build_dir/node_modules"
+  fi
+  ln -s "$build_dir/node_modules" "$build_dir/demeteorized/node_modules"
+  ln -s "$build_dir/demeteorized/package.json" "$build_dir/package.json"
 
   status "Application demeteorized"
 
