7c547178a9fea07afd2b85a2f35888c1c797566d
Hunter Loftis <hunter@hunterloftis.com>
2015-01-07 15:34:05 -0800

    rephrasing

--- bin/warnings.sh
+++ bin/warnings.sh
@@ -14,6 +14,12 @@ elif [ ${node_engine:0:1} == ">" ]; then
   warning "Avoid semver ranges starting with '>' in engines.node" "https://devcenter.heroku.com/articles/nodejs-support#specifying-a-node-js-version"
 fi
 
+if [ "$npm_engine" != "" ]; then
+  if [ "${npm_engine:0:1}" -lt "2" ]; then
+    warning "This version of npm has several known issues - consider upgrading to the latest release"
+  fi
+fi
+
 if [ "$modules_source" == "prebuilt" ]; then
   warning "Avoid checking node_modules into source control" "https://www.npmjs.org/doc/misc/npm-faq.html#should-i-check-my-node_modules-folder-into-git-"
 elif [ "$modules_source" == "" ]; then
@@ -22,10 +28,4 @@ fi
 
 if [ "$start_method" == "" ]; then
   warning "No Procfile, package.json start script, or server.js file found" "https://devcenter.heroku.com/articles/nodejs-support#runtime-behavior"
-fi
-
-if [ "$npm_engine" != "" ]; then
-  if [ "${npm_engine:0:1}" -lt "2" ]; then
-    warning "This version of npm has several known issues - consider upgrading to the latest release"
-  fi
 fi
