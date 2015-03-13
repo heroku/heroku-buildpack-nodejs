0b3c40d6a99c8394332d53183672ba4d58966424
Soulou <leo@unbekandt.eu>
2015-03-13 23:26:14 +0100

    Better warning for meteor

--- lib/warnings.sh
+++ lib/warnings.sh
@@ -1,11 +1,17 @@
 warn_node_engine() {
   local node_engine=$1
-  if [ "$node_engine" == "" ]; then
-    warning "Node version not specified in package.json" "http://doc.scalingo.com/languages/javascript/nodejs"
-  elif [ "$node_engine" == "*" ]; then
-    warning "Avoid semver ranges like '*' in engines.node" "http://doc.scalingo.com/languages/javascript/nodejs"
-  elif [ ${node_engine:0:1} == ">" ]; then
-    warning "Avoid semver ranges starting with '>' in engines.node" "http://doc.scalingo.com/languages/javascript/nodejs"
+  if [ "$meteor_version" != "" ] ; then
+    if [ "${node_engine:0:4}" == "0.11" -o "${node_engine:0:4}" == "0.12" ] ; then
+      warning "Meteor is not compatible with version ${node_engine} of Node.JS" "http://doc.scalingo.com/languages/javascript/nodejs/meteor"
+    fi
+  else
+    if [ "$node_engine" == "" ]; then
+      warning "Node version not specified in package.json" "http://doc.scalingo.com/languages/javascript/nodejs"
+    elif [ "$node_engine" == "*" ]; then
+      warning "Avoid semver ranges like '*' in engines.node" "http://doc.scalingo.com/languages/javascript/nodejs"
+    elif [ ${node_engine:0:1} == ">" ]; then
+      warning "Avoid semver ranges starting with '>' in engines.node" "http://doc.scalingo.com/languages/javascript/nodejs"
+    fi
   fi
 }
 
