88578e32494e977e563acb74862c58a33b8a8462
Hunter Loftis <hunter@hunterloftis.com>
2015-10-06 21:23:50 -0400

    send output through awk

--- lib/output.sh
+++ lib/output.sh
@@ -2,15 +2,15 @@ info() {
   echo "       $*"
 }
 
-# sed's buffer can't hold some of the huge build output from JS asset builders
-# try awk next? awk  '{ print "       " $0 }'
+# sed has a problem with the huge build output from npm 3
+# try awk? awk  '{ print "       " $0 }'
 output() {
   local logfile="$1"
   local c='s/^/       /'
 
   case $(uname) in
-    Darwin) tee -a "$logfile";;
-    *)      tee -a "$logfile";;
+    Darwin) tee -a "$logfile" | awk '{ print "       " $0 }';;
+    *)      tee -a "$logfile" | awk -W interactive '{ print "       " $0 }';;
   esac
 }
 
