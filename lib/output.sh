9444a6f7fe3ae05e1e6e463f1433b498e02a3f52
Hunter Loftis <hunter@hunterloftis.com>
2015-10-08 20:41:04 -0400

    replace tee/sed/awk with a simple echo loop

--- lib/output.sh
+++ lib/output.sh
@@ -2,19 +2,15 @@ info() {
   echo "       $*"
 }
 
-# format output on both OSX and Ubuntu, cedar-10 and cedar-14, while also splitting to a log file
+# format output and send a copy to the log
 output() {
   local logfile="$1"
-  local c='s/^/       /'
 
-  if [ "${STACK:-cedar-14}" = "cedar" ]; then
-    tee -a "$logfile" | awk -W interactive '{ print "       " $0 }'
-  else
-    case $(uname) in
-      Darwin) sed -l "$c" | tee -i -a "$logfile" 2> /dev/null;;
-      *)      stdbuf -oL -eL sed -u "$c" | tee -i -a "$logfile" 2> /dev/null;;
-    esac
-  fi
+  while read LINE;
+  do
+    echo "       $LINE"
+    echo "$LINE" >> "$logfile"
+  done
 }
 
 header() {
