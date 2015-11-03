381f36f2e43ef66ba8a35daaaa3b4b88e6069e0a
Hunter Loftis <hunter@hunterloftis.com>
2015-11-03 09:47:20 -0500

    search node_modules after normal $PATH

--- profile/nodejs.sh
+++ profile/nodejs.sh
@@ -28,7 +28,7 @@ detect_memory() {
   esac
 }
 
-export PATH="$HOME/.heroku/node/bin:$HOME/bin:$HOME/node_modules/.bin:$PATH"
+export PATH="$HOME/.heroku/node/bin:$PATH:$HOME/bin:$HOME/node_modules/.bin"
 export NODE_HOME="$HOME/.heroku/node"
 export NODE_ENV=${NODE_ENV:-production}
 
