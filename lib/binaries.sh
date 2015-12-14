7f2947e3c0a050b09f1556c0f08aa03652253715
Hunter Loftis <hunter@hunterloftis.com>
2015-12-14 17:37:32 -0500

    retry all failed curls; update tests

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -13,12 +13,12 @@ install_nodejs() {
 
   if needs_resolution "$version"; then
     echo "Resolving node version ${version:-(latest stable)} via semver.io..."
-    local version=$(curl --silent --get --data-urlencode "range=${version}" https://semver.herokuapp.com/node/resolve)
+    local version=$(curl --silent --get --retry 5 --retry-max-time 15 --data-urlencode "range=${version}" https://semver.herokuapp.com/node/resolve)
   fi
 
   echo "Downloading and installing node $version..."
   local download_url="http://s3pository.heroku.com/node/v$version/node-v$version-$os-$cpu.tar.gz"
-  curl "$download_url" --silent --fail -o /tmp/node.tar.gz || (echo "Unable to download node $version; does it exist?" && false)
+  curl "$download_url" --silent --fail  --retry 5 --retry-max-time 15 -o /tmp/node.tar.gz || (echo "Unable to download node $version; does it exist?" && false)
   tar xzf /tmp/node.tar.gz -C /tmp
   mv /tmp/node-v$version-$os-$cpu/* $dir
   chmod +x $dir/bin/*
@@ -30,12 +30,12 @@ install_iojs() {
 
   if needs_resolution "$version"; then
     echo "Resolving iojs version ${version:-(latest stable)} via semver.io..."
-    version=$(curl --silent --get --data-urlencode "range=${version}" https://semver.herokuapp.com/iojs/resolve)
+    version=$(curl --silent --get  --retry 5 --retry-max-time 15 --data-urlencode "range=${version}" https://semver.herokuapp.com/iojs/resolve)
   fi
 
   echo "Downloading and installing iojs $version..."
   local download_url="https://iojs.org/dist/v$version/iojs-v$version-$os-$cpu.tar.gz"
-  curl "$download_url" --silent --fail -o /tmp/node.tar.gz || (echo "Unable to download iojs $version; does it exist?" && false)
+  curl "$download_url" --silent --fail --retry 5 --retry-max-time 15 -o /tmp/node.tar.gz || (echo "Unable to download iojs $version; does it exist?" && false)
   tar xzf /tmp/node.tar.gz -C /tmp
   mv /tmp/iojs-v$version-$os-$cpu/* $dir
   chmod +x $dir/bin/*
