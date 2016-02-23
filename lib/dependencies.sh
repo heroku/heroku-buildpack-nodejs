5085334c52d5790cdb0284441a29d55c279873de
Tom Spencer <fiznool@gmail.com>
2016-02-23 21:36:29 +0000

    Added heroku prebuild and postbuild scripts

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -1,8 +1,14 @@
 install_node_modules() {
   local build_dir=${1:-}
+  local has_prebuild_script=${2:-}
+  local has_postbuild_script=${3:-}
 
   if [ -e $build_dir/package.json ]; then
     cd $build_dir
+    if [ -n "$has_prebuild_script" ]; then
+      echo "Running prebuild script"
+      npm run heroku-prebuild
+    fi
     echo "Pruning any extraneous modules"
     npm prune --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
     if [ -e $build_dir/npm-shrinkwrap.json ]; then
@@ -11,6 +17,10 @@ install_node_modules() {
       echo "Installing node modules (package.json)"
     fi
     npm install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
+    if [ -n "$has_postbuild_script" ]; then
+      echo "Running postbuild script"
+      npm run heroku-postbuild
+    fi
   else
     echo "Skipping (no package.json)"
   fi
@@ -18,9 +28,15 @@ install_node_modules() {
 
 rebuild_node_modules() {
   local build_dir=${1:-}
+  local has_prebuild_script=${2:-}
+  local has_postbuild_script=${3:-}
 
   if [ -e $build_dir/package.json ]; then
     cd $build_dir
+    if [ -n "$has_prebuild_script" ]; then
+      echo "Running prebuild script"
+      npm run heroku-prebuild
+    fi
     echo "Rebuilding any native modules"
     npm rebuild 2>&1
     if [ -e $build_dir/npm-shrinkwrap.json ]; then
@@ -29,6 +45,10 @@ rebuild_node_modules() {
       echo "Installing any new modules (package.json)"
     fi
     npm install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
+    if [ -n "$has_postbuild_script" ]; then
+      echo "Running postbuild script"
+      npm run heroku-postbuild
+    fi
   else
     echo "Skipping (no package.json)"
   fi
