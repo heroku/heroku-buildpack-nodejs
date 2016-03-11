649802e76d3b7e4f61917ee5034c67c52309500d
Hunter Loftis <hunter@hunterloftis.com>
2016-03-11 12:28:14 -0500

    simplify a bit

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -1,3 +1,12 @@
+run_if_present() {
+  local script_name=${1:-}
+  local has_script=$(read_json "$BUILD_DIR/package.json" ".scripts[\"$script_name\"]")
+  if [ -n "$has_script" ]; then
+    echo "Running $script_name"
+    npm run "$script_name" --if-present
+  fi
+}
+
 install_node_modules() {
   local build_dir=${1:-}
   local has_prebuild_script=${2:-}
@@ -5,10 +14,7 @@ install_node_modules() {
 
   if [ -e $build_dir/package.json ]; then
     cd $build_dir
-    if [ -n "$has_prebuild_script" ]; then
-      echo "Running prebuild script"
-      npm run heroku-prebuild
-    fi
+
     echo "Pruning any extraneous modules"
     npm prune --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
     if [ -e $build_dir/npm-shrinkwrap.json ]; then
