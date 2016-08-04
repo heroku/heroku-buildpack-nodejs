54206d330f2fc3eac5e14bf1269f3f8513d81fbc
Hunter Loftis <hunter@hunterloftis.com>
2016-08-04 16:03:27 -0400

    reset cache on prebuild change or stack change

--- lib/cache.sh
+++ lib/cache.sh
@@ -1,7 +1,7 @@
 source $BP_DIR/lib/binaries.sh
 
 create_signature() {
-  echo "$(node --version); $(npm --version)"
+  echo "$(node -v); $(npm -v); ${PREBUILD:-''}; ${STACK:-''};"
 }
 
 save_signature() {
