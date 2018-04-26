7a6adffaa03c8ce463b0e8f2f5fc62512275f0de
Joe Kutner <jpkutner@gmail.com>
2018-04-26 10:37:45 -0500

    Only write export script if dir is writable

--- lib/environment.sh
+++ lib/environment.sh
@@ -73,6 +73,12 @@ write_ci_profile() {
 write_export() {
   local bp_dir="$1"
   local build_dir="$2"
-  echo "export PATH=\"$build_dir/.heroku/node/bin:$build_dir/.heroku/yarn/bin:\$PATH:$build_dir/node_modules/.bin\"" > $bp_dir/export
-  echo "export NODE_HOME=\"$build_dir/.heroku/node\"" >> $bp_dir/export
+
+  # only write the export script if the buildpack directory is writable.
+  # this may occur in situations outside of Heroku, such as running the
+  # buildpacks locally.
+  if [ -w ${bp_dir} ]; then
+    echo "export PATH=\"$build_dir/.heroku/node/bin:$build_dir/.heroku/yarn/bin:\$PATH:$build_dir/node_modules/.bin\"" > $bp_dir/export
+    echo "export NODE_HOME=\"$build_dir/.heroku/node\"" >> $bp_dir/export
+  fi
 }
