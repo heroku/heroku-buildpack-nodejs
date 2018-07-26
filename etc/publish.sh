8ed4c0a32ed91232d03e2f946adb70f0b41fb4f6
Jeremy Morrell <morrell.jeremy@gmail.com>
2018-07-26 11:00:16 -0700

    Add buildpack.toml and publish script (#548)
    Add buildpack.toml and publish script

---
+++ etc/publish.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+BP_NAME=${1:-"heroku/nodejs"}
+
+curVersion=$(heroku buildpacks:versions "$BP_NAME" | awk 'FNR == 3 { print $1 }')
+newVersion="v$((curVersion + 1))"
+
+read -p "Deploy as version: $newVersion [y/n]? " choice
+case "$choice" in
+  y|Y ) echo "";;
+  n|N ) exit 0;;
+  * ) exit 1;;
+esac
+
+originMaster=$(git rev-parse origin/master)
+echo "Tagging commit $originMaster with $newVersion... "
+git tag "$newVersion" "${originMaster:?}"
+git push origin refs/tags/$newVersion
+
+heroku buildpacks:publish "$BP_NAME" "$newVersion"
+
+echo "Done."
