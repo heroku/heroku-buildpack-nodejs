7bc126070237144c3061dd9369cd6aa38f29d29d
Jeremy Morrell <jmorrell@heroku.com>
2018-08-21 21:58:18 -0700

    Add metrics plugin + unit tests
    This merges in the build changes and plugin from https://github.com/heroku/heroku-nodejs-metrics-buildpack
    which installs the metrics plugin https://github.com/heroku/heroku-nodejs-plugin at runtime once a
    Heroku use opts in to the language metrics feature

---
+++ lib/plugin.sh
@@ -0,0 +1,24 @@
+
+get_node_major_version() {
+  local node_version="$(node --version)"
+  # major_string will be ex: "6." "8." "10"
+  local major_string=${node_version:1:2}
+  # strip any "."s from major_string
+  local major=${major_string//.}
+  echo $major
+}
+
+install_plugin() {
+  local bp_dir="$1"
+  local build_dir="$2"
+  local major=$(get_node_major_version)
+  local plugin="${bp_dir}/plugin/heroku-nodejs-plugin-node-${major}.tar.gz"
+
+  # If we have a version of the plugin compiled for this version of node, and the
+  # user has not opted out of including the plugin, copy it into the slug.
+  # It will be included at runtime once the user opts into the Node metrics feature
+  if [[ -f "${plugin}" ]] && [[ -z "$HEROKU_SKIP_NODE_PLUGIN" ]]; then
+    mkdir -p "${build_dir}/.heroku/"
+    tar -xzf ${plugin} -C "${build_dir}/.heroku/"
+  fi
+}
