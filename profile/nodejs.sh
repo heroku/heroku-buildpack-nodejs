4b3fdc0778488ba46378ed34b5f26adb0aab04bc
Jeremy Morrell <jmorrell@heroku.com>
2018-08-24 14:49:28 -0700

    Fix issue with old Node and metrics plugin
    The resolves an issue where a user that is not using a version
    of Node supported by the metrics plugin (not 8, 9, or 10) opts
    in to the feature. Since the plugin for the appropriate version
    of Node is copied into the slug at build time, but the metrics
    url is set at runtime, we need to verify that the plugin exists
    before including it.

--- profile/nodejs.sh
+++ profile/nodejs.sh
@@ -2,9 +2,14 @@ export PATH="$HOME/.heroku/node/bin:$HOME/.heroku/yarn/bin:$PATH:$HOME/bin:$HOME
 export NODE_HOME="$HOME/.heroku/node"
 export NODE_ENV=${NODE_ENV:-production}
 
-# If the metrics url is not present, this is the wrong type of dyno, or the user has opted out, 
-# don't include the metrics plugin
-if [[ -n "$HEROKU_METRICS_URL" ]] && [[ "${DYNO}" != run\.* ]] && [[ -z "$HEROKU_SKIP_NODE_PLUGIN" ]]; then
+# If the user has opted into the feature
+if [[ -n "$HEROKU_METRICS_URL" ]] && \
+   # if we're not on a one-off dyno
+   [[ "${DYNO}" != run\.* ]] && \
+   # if the plugin was installed for this node version
+   [[ -d $HOME/.heroku/heroku-nodejs-plugin ]] && \
+   # the user has not opted out
+   [[ -z "$HEROKU_SKIP_NODE_PLUGIN" ]]; then
 
   # Don't clobber NODE_OPTIONS if the user has set it, just add the require flag to the end
   if [[ -z "$NODE_OPTIONS" ]]; then
