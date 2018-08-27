0a06946f28406180077aca69579a316b25190097
Jeremy Morrell <morrell.jeremy@gmail.com>
2018-08-27 09:28:47 -0700

    Hatchet tests (#552)
    Adds hatchet tests as part of our testing matrix and adds that to Travis CI

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+
+gem 'heroku_hatchet', :git => 'https://github.com/heroku/hatchet.git', :branch => 'support-local-directories-for-fixtures'
+gem 'rspec-retry'
+gem 'rspec-expectations'
+gem 'sem_version'
+gem "parallel_tests"
+gem "rake"
---
+++ spec/spec_helper.rb
@@ -0,0 +1,34 @@
+require 'rspec/core'
+require 'hatchet'
+require 'fileutils'
+require 'hatchet'
+require 'rspec/retry'
+require 'date'
+require 'json'
+require 'sem_version'
+
+ENV['RACK_ENV'] = 'test'
+
+RSpec.configure do |config|
+  config.filter_run focused: true unless ENV['IS_RUNNING_ON_TRAVIS']
+  config.run_all_when_everything_filtered = true
+  config.alias_example_to :fit, focused: true
+  config.full_backtrace      = true
+  config.verbose_retry       = true # show retry status in spec process
+  config.default_retry_count = 2 if ENV['IS_RUNNING_ON_TRAVIS'] # retry all tests that fail again
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
+
+def successful_body(app, options = {})
+  retry_limit = options[:retry_limit] || 100 
+  path = options[:path] ? "/#{options[:path]}" : ''
+  Excon.get("http://#{app.name}.herokuapp.com#{path}", :idempotent => true, :expects => 200, :retry_limit => retry_limit).body
+end
+
+def successful_json_body(app, options = {})
+  body = successful_body(app, options)
+  JSON.parse(body)
+end
