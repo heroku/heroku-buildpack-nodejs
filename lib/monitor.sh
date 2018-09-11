e6af42e9fb4f9e2adee0e47b447708a85c4cf7e1
Jeremy Morrell <jmorrell@heroku.com>
2018-09-11 13:07:31 -0700

    changes in response to feedback

--- lib/monitor.sh
+++ lib/monitor.sh
@@ -1,13 +1,18 @@
 
 monitor_memory_usage() {
-  local output_file=$1
-  local command="${@:2}"
+  local output_file="$1"
+
+  # drop the first argument, and leave other arguments in place
+  shift
 
   # Run the command in the background
-  exec $command &
+  exec "${@:-}" &
 
   # save the PID of the running command
   pid=$!
+
+  # if this build process is SIGTERM'd
+  trap "kill -TERM $pid" TERM
 
   # set the peak memory usage to 0 to start
   peak="0"
@@ -27,6 +32,10 @@ monitor_memory_usage() {
   echo "$(($peak / 1024))" > $output_file
 
   # After wait returns we can get the exit code of $command
+  wait $pid
+
+  # wait a second time in case the trap was executed
+  # http://veithen.github.io/2014/11/16/sigterm-propagation.html
   wait $pid
 
   # return the exit code of $command
