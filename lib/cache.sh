8e7773b321bdd4111818f8fae27aab5f5908bb2b
Jeremy Morrell <jmorrell@heroku.com>
2018-11-11 17:18:06 -0800

    Move cached directories one level down within the node cache

--- lib/cache.sh
+++ lib/cache.sh
@@ -46,10 +46,10 @@ restore_default_cache_directories() {
   # node_modules
   if [[ -e "$build_dir/node_modules" ]]; then
     echo "- node_modules is checked into source control and cannot be cached"
-  elif [[ -e "$cache_dir/node/node_modules" ]]; then
+  elif [[ -e "$cache_dir/node/cache/node_modules" ]]; then
     echo "- node_modules"
     mkdir -p "$(dirname "$build_dir/node_modules")"
-    mv "$cache_dir/node/node_modules" "$build_dir/node_modules"
+    mv "$cache_dir/node/cache/node_modules" "$build_dir/node_modules"
   else
     echo "- node_modules (not cached - skipping)"
   fi
@@ -71,10 +71,10 @@ restore_custom_cache_directories() {
     if [ -e "$build_dir/$cachepath" ]; then
       echo "- $cachepath (exists - skipping)"
     else
-      if [ -e "$cache_dir/node/$cachepath" ]; then
+      if [ -e "$cache_dir/node/cache/$cachepath" ]; then
         echo "- $cachepath"
         mkdir -p "$(dirname "$build_dir/$cachepath")"
-        mv "$cache_dir/node/$cachepath" "$build_dir/$cachepath"
+        mv "$cache_dir/node/cache/$cachepath" "$build_dir/$cachepath"
       else
         echo "- $cachepath (not cached - skipping)"
       fi
@@ -85,6 +85,7 @@ restore_custom_cache_directories() {
 clear_cache() {
   rm -rf $CACHE_DIR/node
   mkdir -p $CACHE_DIR/node
+  mkdir -p $CACHE_DIR/node/cache
 }
 
 save_default_cache_directories() {
@@ -95,7 +96,7 @@ save_default_cache_directories() {
   if [[ -e "$build_dir/node_modules" ]]; then
     echo "- node_modules"
     mkdir -p "$cache_dir/node/node_modules"
-    cp -a "$build_dir/node_modules" "$(dirname "$cache_dir/node/node_modules")"
+    cp -a "$build_dir/node_modules" "$(dirname "$cache_dir/node/cache/node_modules")"
   else
     # this can happen if there are no dependencies
     mcount "cache.no-node-modules"
@@ -106,8 +107,8 @@ save_default_cache_directories() {
   if [[ -e "$build_dir/bower_components" ]]; then
     mcount "cache.saved-bower-components"
     echo "- bower_components"
-    mkdir -p "$cache_dir/node/bower_components"
-    cp -a "$build_dir/bower_components" "$(dirname "$cache_dir/node/bower_components")"
+    mkdir -p "$cache_dir/node/cache/bower_components"
+    cp -a "$build_dir/bower_components" "$(dirname "$cache_dir/node/cache/bower_components")"
   fi
 }
 
@@ -121,8 +122,8 @@ save_custom_cache_directories() {
   for cachepath in "${cache_directories[@]}"; do
     if [ -e "$build_dir/$cachepath" ]; then
       echo "- $cachepath"
-      mkdir -p "$cache_dir/node/$cachepath"
-      cp -a "$build_dir/$cachepath" "$(dirname "$cache_dir/node/$cachepath")"
+      mkdir -p "$cache_dir/node/cache/$cachepath"
+      cp -a "$build_dir/$cachepath" "$(dirname "$cache_dir/node/cache/$cachepath")"
     else
       echo "- $cachepath (nothing to cache)"
     fi
