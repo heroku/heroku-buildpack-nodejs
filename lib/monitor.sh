039f64064dc89bb95d13bf72adde1ea63b7e995a
Jeremy Morrell <jmorrell@heroku.com>
2018-12-14 13:32:15 -0800

    Fix some deficiencies in the monitor function

--- lib/monitor.sh
+++ lib/monitor.sh
@@ -45,12 +45,13 @@ monitor_memory_usage() {
 monitor() {
   local command_name=$1
   shift
-  local command="${@:-}"
+
+  local command=( "$@" )
   local peak_mem_output=$(mktemp)
   local start=$(nowms)
 
   # execute the subcommand and save the peak memory usage
-  monitor_memory_usage $peak_mem_output $command
+  monitor_memory_usage $peak_mem_output "${command[@]}"
 
   mtime "exec.$command_name.time" "${start}"
   mmeasure "exec.$command_name.memory" "$(cat $peak_mem_output)"
