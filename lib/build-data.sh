2ba62a2d748a7953ddbf275b23c5bc3ba10dec14
Jeremy Morrell <jmorrell@heroku.com>
2018-12-14 18:13:09 -0800

    Quote arguments

--- lib/build-data.sh
+++ lib/build-data.sh
@@ -2,17 +2,17 @@
 BUILD_DATA_FILE=""
 
 bd_create() {
-  local cache_dir=$1
+  local cache_dir="$1"
   BUILD_DATA_FILE="$cache_dir/build-data/node"
   kv_create $BUILD_DATA_FILE
 }
 
 bd_get() {
-  kv_get $BUILD_DATA_FILE $1
+  kv_get $BUILD_DATA_FILE "$1"
 }
 
 bd_set() {
-  kv_set $BUILD_DATA_FILE $1 $2 
+  kv_set $BUILD_DATA_FILE "$1" "$2"
 }
 
 log_build_data() {
