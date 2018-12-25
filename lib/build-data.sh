a40ebc70696bdd009bbdf8f919fae21087ac473a
Jeremy Morrell <jmorrell@heroku.com>
2018-12-24 22:13:09 -0600

    Add lib/build-data.sh and lib/cache.sh to shellcheck

--- lib/build-data.sh
+++ lib/build-data.sh
@@ -1,22 +1,24 @@
+#!/usr/bin/env bash
+
 # variable shared by this whole module
 BUILD_DATA_FILE=""
 
 bd_create() {
   local cache_dir="$1"
   BUILD_DATA_FILE="$cache_dir/build-data/node"
-  kv_create $BUILD_DATA_FILE
+  kv_create "$BUILD_DATA_FILE"
 }
 
 bd_get() {
-  kv_get $BUILD_DATA_FILE "$1"
+  kv_get "$BUILD_DATA_FILE" "$1"
 }
 
 bd_set() {
-  kv_set $BUILD_DATA_FILE "$1" "$2"
+  kv_set "$BUILD_DATA_FILE" "$1" "$2"
 }
 
 log_build_data() {
   # print all values on one line in logfmt format
   # https://brandur.org/logfmt
-  echo $(kv_list $BUILD_DATA_FILE)
+  kv_list "$BUILD_DATA_FILE"
 }
