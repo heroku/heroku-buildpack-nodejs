0ebcf5ea77627598af212627fa99c87ad07ced17
Jeremy Morrell <jmorrell@heroku.com>
2019-01-25 12:24:59 -0800

    Fail CI if no tests are defined

--- lib/output.sh
+++ lib/output.sh
@@ -32,3 +32,19 @@ error() {
   echo " !     $*" >&2 || true
   echo "" || true
 }
+
+pipeError() {
+  local c="2,999 s/^/ !     /"
+	# send all of our output to stderr
+	exec 1>&2
+
+	echo -e "\033[1;31m" # bold; red
+	echo -n " !     ERROR: "
+	# this will be fed from stdin
+  case $(uname) in
+		Darwin) sed -l "$c";; # mac/bsd sed: -l buffers on line boundaries
+		*)      sed -u "$c";; # unix/gnu sed: -u unbuffered (arbitrary) chunks of data
+	esac
+	echo -e "\033[0m" # reset style
+	exit 1
+}
