ca50689793f90eb1c006ea01428f8075e0305945
Jeremy Morrell <jmorrell@heroku.com>
2019-02-28 18:31:31 -0800

    Run the build script by default
    This removes the opt-in behavior and defaults users into running
    the "build" script by default. Users that were opted-in will see
    no change

--- lib/metadata.sh
+++ lib/metadata.sh
@@ -23,31 +23,3 @@ generate_uuids() {
     bd_set "app-uuid" "$(uuid)"
   fi
 }
-
-log_build_script_opt_in() {
-  local opted_in="$1"
-  local build_dir="$2"
-  local has_build_script has_heroku_build_script
-
-  has_build_script=$(read_json "$build_dir/package.json" ".scripts.build")
-  has_heroku_build_script=$(read_json "$build_dir/package.json" ".scripts[\"heroku-postbuild\"]")
-
-  # if this app will be affected by the change
-  if [[ -z "$has_heroku_build_script" ]] && [[ -n "$has_build_script" ]]; then
-    mcount "affected-by-build-change"
-
-    if [[ "$opted_in" = "true" ]]; then
-      mcount "affected-by-build-change-opted-in"
-      bd_set "affected-but-opted-in" "true"
-    else
-      bd_set "affected-but-opted-in" "false"
-    fi
-
-  fi
-
-  if [[ "$opted_in" = true ]]; then
-    bd_set "build-script-opt-in" "true"
-  else
-    bd_set "build-script-opt-in" "false"
-  fi
-}
