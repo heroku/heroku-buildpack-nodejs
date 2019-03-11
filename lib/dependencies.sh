827e960b029944f85c854ee9980a78f372ab74bd
Jeremy Morrell <morrell.jeremy@gmail.com>
2019-03-11 06:27:37 -0700

    Run the build script by default (#628)
    * Run the build script by default
    
    This removes the opt-in behavior and defaults users into running
    the "build" script by default. Users that were opted-in will see
    no change

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -60,41 +60,6 @@ run_build_script() {
   fi
 }
 
-warn_build_script_behavior_opt_in() {
-  local opted_in="$1"
-  if [[ "$opted_in" = true ]]; then
-    header "Opting in to new default build script behavior"
-    echo "You have set \"heroku-run-build-script\"=true in your package.json"
-    echo "Your app will be unaffected by the change on March 11, 2019"
-    echo ""
-    mcount "build-change-opt-in"
-  fi
-}
-
-warn_build_script_behavior_change() {
-  local opted_in="$1"
-  local build_dir="$2"
-  local has_build_script has_heroku_build_script
-
-  has_build_script=$(read_json "$build_dir/package.json" ".scripts.build")
-  has_heroku_build_script=$(read_json "$build_dir/package.json" ".scripts[\"heroku-postbuild\"]")
-
-  if [[ -z "$has_heroku_build_script" ]] && [[ -n "$has_build_script" ]] && [[ "$opted_in" != "true" ]]; then
-    bright_header "Change to Node.js build process"
-    echo "On March 11, 2019 Heroku will begin executing the \"build\" script defined in package.json"
-    echo "by default. This application may be affected by this change."
-    echo ""
-    echo "To make this transition easier, we've published a tool that will automatically" 
-    echo "update your app for you. You can run it with one command in your app's"
-    echo "root directory:"
-    echo ""
-    echo "$ npx @heroku/update-node-build-script"
-    echo ""
-    echo "Please see https://help.heroku.com/P5IMU3MP/heroku-node-js-build-script-change-faq for more information"
-    echo ""
-  fi
-}
-
 log_build_scripts() {
   local build heroku_prebuild heroku_postbuild postinstall
   local build_dir=${1:-}
