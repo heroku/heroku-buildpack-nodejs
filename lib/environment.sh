3ea6d3f6cebfd490149c42291205ec9f81621c3f
Jeremy Morrell <jmorrell@heroku.com>
2019-03-27 14:01:30 -0700

    Blacklist env vars with dots

--- lib/environment.sh
+++ lib/environment.sh
@@ -56,7 +56,7 @@ export_env_dir() {
   local env_dir=$1
   if [ -d "$env_dir" ]; then
     local whitelist_regex=${2:-''}
-    local blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH|LANG|BUILD_DIR)$'}
+    local blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH|LANG|BUILD_DIR|.*\..*)$'}
     # shellcheck disable=SC2164
     pushd "$env_dir" >/dev/null
     for e in *; do
