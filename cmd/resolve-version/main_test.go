db1af655f0d02aee75b2f6c399e7b62a3279109d
Jeremy Morrell <jmorrell@heroku.com>
2019-04-16 12:00:17 -0700

    Add logic for fetching S3 objects

--- cmd/resolve-version/main_test.go
+++ cmd/resolve-version/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -38,4 +39,42 @@ func TestParseObject(t *testing.T) {
 	release, err = parseObject("something/weird")
 	assert.NotNil(t, err)
 	assert.Equal(t, err.Error(), "Failed to parse key: something/weird")
+}
+
+func TestListS3Objects(t *testing.T) {
+	// Node
+	objects, err := listS3Objects("heroku-nodebin", "node")
+	assert.Nil(t, err)
+	assert.NotEmpty(t, objects)
+
+	// every returned result started with "node"
+	for _, obj := range objects {
+		assert.Regexp(t, regexp.MustCompile("^node"), obj.Key)
+	}
+
+	// every node object must parse as a valid release
+	for _, obj := range objects {
+		release, err := parseObject(obj.Key)
+		assert.Nil(t, err)
+		assert.Regexp(t, regexp.MustCompile("https:\\/\\/s3.amazonaws.com\\/heroku-nodebin"), release.url)
+		assert.Regexp(t, regexp.MustCompile("[0-9]+.[0-9]+.[0-9]+"), release.version.String())
+	}
+
+	// Yarn
+	objects, err = listS3Objects("heroku-nodebin", "yarn")
+	assert.Nil(t, err)
+	assert.NotEmpty(t, objects)
+
+	// every returned result started with "yarn"
+	for _, obj := range objects {
+		assert.Regexp(t, regexp.MustCompile("^yarn"), obj.Key)
+	}
+
+	// every yarn object must parse as a valid release
+	for _, obj := range objects {
+		release, err := parseObject(obj.Key)
+		assert.Nil(t, err)
+		assert.Regexp(t, regexp.MustCompile("https:\\/\\/s3.amazonaws.com\\/heroku-nodebin"), release.url)
+		assert.Regexp(t, regexp.MustCompile("[0-9]+.[0-9]+.[0-9]+"), release.version.String())
+	}
 }
