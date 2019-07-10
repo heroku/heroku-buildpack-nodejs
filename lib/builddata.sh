83525e84b5c0e823c2bf75436198e43351432106
Jeremy Morrell <morrell.jeremy@gmail.com>
2019-07-10 13:23:53 -0700

    Add logging to track yarn workspace usage (#685)
    * Add logging to track workspace usage

--- lib/builddata.sh
+++ lib/builddata.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Log out information about the build that we can detect without accessing package.json
 log_initial_state() {
   meta_set "buildpack" "nodejs"
   if "$YARN"; then
@@ -19,6 +20,16 @@ log_initial_state() {
       meta_set "experiment-$key" "$(experiments_get "$key")"
     fi
   done
+}
+
+# Log out information about the build that we can read from package.json
+log_project_info() {
+  local build_dir="$1"
+
+  # Does this project use "workspaces"?
+  meta_set "uses-workspaces" "$(json_has_key "$build_dir/package.json" "workspaces")"
+  # What workspaces are defined? Logs as: `["packages/*","a","b"]`
+  meta_set "workspaces" "$(read_json "$build_dir/package.json" ".workspaces")"
 }
 
 generate_uuids() {
