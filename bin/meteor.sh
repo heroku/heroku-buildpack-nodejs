a24906cd6499ad322ec3b0e82a5e090510a3348a
Soulou <leo@unbekandt.eu>
2019-12-02 18:00:34 +0100

    Start differently apps using meteor 1.8, since a new start script exists

--- bin/meteor.sh
+++ bin/meteor.sh
@@ -91,11 +91,19 @@ EOF
 
 create_meteor_startup_file() {
   local build_dir=$1
+  local minor=$(get_meteor_minor_version)
 
-  cat << EOF > "${build_dir}/.start-meteor-app"
+  if [ "$minor" -lt 8 ] ; then
+    cat << EOF > "${build_dir}/.start-meteor-app"
 cd .app-build/bundle/programs/server
 exec node \$NODE_BOOT_FLAGS boot.js program.json
 EOF
+  else
+    cat << EOF > "${build_dir}/.start-meteor-app"
+cd .app-build/bundle
+exec node \$NODE_BOOT_FLAGS main.js
+EOF
+  fi
 
   chmod +x "${build_dir}/.start-meteor-app"
 }
