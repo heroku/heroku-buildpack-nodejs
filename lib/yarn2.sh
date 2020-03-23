ec53927495cb7689866a8101a7cb6c82fbc677d1
Danielle Adams <danielle.adams@heroku.com>
2020-03-23 17:03:18 -0400

    fix shellcheck issues

--- lib/yarn2.sh
+++ lib/yarn2.sh
@@ -5,8 +5,9 @@ YQ="$BP_DIR/vendor/yq-$(get_os)"
 detect_yarn2() {
   local uses_yarn="$1"
   local build_dir="$2"
+  local is_yml
 
-  local is_yml=$($YQ v "$build_dir/yarn.lock" 2>&1)
+  is_yml=$($YQ v "$build_dir/yarn.lock" 2>&1)
 
   if [[ "$uses_yarn" == "true" && "$is_yml" == "" ]]; then
     echo "true"
