95beb5075b874882894cbd23da422f879b1775c4
Danielle Adams <danielle.adams@heroku.com>
2020-04-27 08:53:12 -0400

    add spec support for Node 13 and 14

---
+++ spec/ci/node_14_metrics_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../spec_helper'
+
+describe "Node Metrics for v14.x" do
+  context "test metrics for Node v14.x app" do
+    let(:app) {
+      Hatchet::Runner.new(
+        "spec/fixtures/repos/node-14-metrics",
+        config: {
+          "HEROKU_METRICS_URL" => "http://localhost:3000",
+          "METRICS_INTERVAL_OVERRIDE" => "10000"
+        }
+      )
+    }
+
+    it "should deploy" do
+      app.deploy do |app|
+        data = successful_json_body(app)
+        expect(data["gauges"]["node.eventloop.delay.ms.max"]).to be >= 2000
+        expect(data["counters"]["node.gc.collections"]).to be >= 0
+        expect(data["counters"]["node.gc.young.collections"]).to be >= 0
+        expect(data["counters"]["node.gc.old.collections"]).to be >= 0
+      end
+    end
+  end
+end
