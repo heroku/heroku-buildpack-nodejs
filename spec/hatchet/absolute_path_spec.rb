d56a7f96267b8ff265d1ef215f4f6afb25ecdae0
Lillian Zhang <zhang.l@salesforce.com>
2020-08-20 11:22:53 -0400

    update absolute path spec

--- spec/hatchet/absolute_path_spec.rb
+++ spec/hatchet/absolute_path_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../spec_helper'
 
 #Test that all paths set by the buildpack are absolute instead of relative
-describe "Rails 5.1" do
-  it "works with webpacker + yarn (js friends)" do
+describe "Buildpack paths" do
+  it "are absolute" do
     buildpacks = [
       ENV["HATCHET_BUILDPACK_BASE"],
       "https://github.com/sharpstone/force_absolute_paths_buildpack"
