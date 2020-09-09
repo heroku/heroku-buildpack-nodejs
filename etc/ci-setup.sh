e5c2936ccc9a1e7e2715ad79779705c461e23f7d
Danielle Adams <danielle.adams@heroku.com>
2020-09-09 12:48:14 -0400

    exit hatchet test on forks

--- etc/ci-setup.sh
+++ etc/ci-setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 [ "$CI" != "true" ] && echo "Not running on CI!" && exit 1
+[ "$TRAVIS_PULL_REQUEST_SLUG" != "heroku/heroku-buildpack-nodejs" ] && echo "Run tests manually for forked PRs." && exit 0
 
 bundle install
 bundle exec hatchet ci:setup
