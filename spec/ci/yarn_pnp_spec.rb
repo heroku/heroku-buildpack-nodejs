a63e1e37634ab78663a6b676acfa4ddf94987ef9
Josh W Lewis <josh.lewis@salesforce.com>
2022-02-03 14:21:39 -0600

    Use correct ENV var for pnp spec

--- spec/ci/yarn_pnp_spec.rb
+++ spec/ci/yarn_pnp_spec.rb
@@ -4,7 +4,7 @@ describe "yarn pnp hello world" do
   it "zero-install with NODE_MODLES_CACHE=false should deploy successfully" do
     app = Hatchet::Runner.new(
       "spec/fixtures/repos/yarn-pnp-zero-install",
-      config: { "NODE_MODLES_CACHE": "false" }
+      config: { "NODE_MODULES_CACHE": "false" }
     )
     app.deploy do |app|
       expect(successful_body(app).strip).to eq("Hello from yarn-pnp-zero-install")
