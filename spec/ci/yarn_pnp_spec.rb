53cd8ff6322c99200aa2d3551d804439df163fce
Josh W Lewis <josh.lewis@salesforce.com>
2022-02-03 14:07:39 -0600

    set NODE_MODULES_CACHE=false when testing pnp zero-install

--- spec/ci/yarn_pnp_spec.rb
+++ spec/ci/yarn_pnp_spec.rb
@@ -1,15 +1,13 @@
 require_relative '../spec_helper'
 
 describe "yarn pnp hello world" do
-  context "a single process zero-install app" do
-    let(:app) {
-      Hatchet::Runner.new("spec/fixtures/repos/yarn-pnp-zero-install")
+  it "zero-install with NODE_MODLES_CACHE=false should deploy successfully" do
+    app = Hatchet::Runner.new(
+      "spec/fixtures/repos/yarn-pnp-zero-install",
+      config: { "NODE_MODLES_CACHE": "false" }
     }
-
-    it "should deploy successfully" do
-      app.deploy do |app|
-        expect(successful_body(app).strip).to eq("Hello from yarn-pnp-zero-install")
-      end
+    app.deploy do |app|
+      expect(successful_body(app).strip).to eq("Hello from yarn-pnp-zero-install")
     end
   end
 end
