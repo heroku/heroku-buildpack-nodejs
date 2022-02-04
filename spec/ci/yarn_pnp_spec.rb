a3e818825678644bb152a533c923787eb755ff4f
Josh W Lewis <josh.lewis@salesforce.com>
2022-02-04 16:52:29 -0600

    Fix first deploy expectations

--- spec/ci/yarn_pnp_spec.rb
+++ spec/ci/yarn_pnp_spec.rb
@@ -4,7 +4,6 @@ describe "yarn pnp hello world" do
   it "zero-install with should deploy successfully and not use the cache" do
     app = Hatchet::Runner.new("spec/fixtures/repos/yarn-pnp-zero-install")
     app.deploy do |app|
-      expect(app.output).to include("yarn cache (not cached - skipping)")
       expect(successful_body(app).strip).to eq("Hello from yarn-pnp-zero-install")
 
       run!('git commit --allow-empty -m "deploy again to test cache reuse"')
@@ -18,12 +17,12 @@ describe "yarn pnp hello world" do
   it "nonzero-install should deploy successfully and reuse the cache" do
     app = Hatchet::Runner.new("spec/fixtures/repos/yarn-pnp-nonzero-install")
     app.deploy do |app|
-      expect(app.output).to include("yarn cache (not cached - skipping)")
       expect(successful_body(app).strip).to eq("Hello from yarn-pnp-nonzero-install")
 
       run!('git commit --allow-empty -m "deploy again to test cache reuse"')
       app.push!
 
+      expect(app.output).to include("Restoring cache")
       expect(app.output).to include("- yarn cache")
       expect(successful_body(app).strip).to eq("Hello from yarn-pnp-nonzero-install")
     end
