44c90ec4143f71e5c52aa87151c921b833865bdf
Josh W Lewis <josh.lewis@salesforce.com>
2022-02-04 15:34:31 -0600

    Test pnp nonzero install cache reuse

--- spec/ci/yarn_pnp_spec.rb
+++ spec/ci/yarn_pnp_spec.rb
@@ -11,12 +11,19 @@ describe "yarn pnp hello world" do
     end
   end
 
-  it "nonzero-install should deploy successfully" do
+  it "nonzero-install should deploy successfully and reuse the cache" do
     app = Hatchet::Runner.new(
       "spec/fixtures/repos/yarn-pnp-nonzero-install",
       config: { "NODE_MODULES_CACHE": "false" }
     )
     app.deploy do |app|
+      expect(successful_body(app).strip).to eq("Hello from yarn-pnp-nonzero-install")
+
+      run!('git commit --allow-empty -m "deploy again to test cache reuse"')
+      app.push!
+
+      expect(app.output).to include("Restoring cache")
+      expect(app.output).to include("- yarn cache")
       expect(successful_body(app).strip).to eq("Hello from yarn-pnp-nonzero-install")
     end
   end
