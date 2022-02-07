45d1b047a222ec3f7764b2bf0f28960d6f064d3e
Josh W Lewis <josh.lewis@salesforce.com>
2022-02-07 14:49:20 -0600

    Add a spec for yarn 2 using node_modules nonzero install

--- spec/ci/yarn2_spec.rb
+++ spec/ci/yarn2_spec.rb
@@ -4,27 +4,41 @@ describe "yarn2 examples" do
   it "pnp zero-install with should deploy successfully and not use the cache" do
     app = Hatchet::Runner.new("spec/fixtures/repos/yarn2-pnp-zero-install")
     app.deploy do |app|
-      expect(successful_body(app).strip).to eq("Hello from yarn-pnp-zero-install")
+      expect(successful_body(app).strip).to eq("Hello from yarn2-pnp-zero-install")
 
       run!('git commit --allow-empty -m "deploy again to test cache reuse"')
       app.push!
 
       expect(app.output).to include("yarn cache is checked into source control and cannot be cached")
-      expect(successful_body(app).strip).to eq("Hello from yarn-pnp-zero-install")
+      expect(successful_body(app).strip).to eq("Hello from yarn2-pnp-zero-install")
     end
   end
 
   it "pnp nonzero-install should deploy successfully and reuse the cache" do
     app = Hatchet::Runner.new("spec/fixtures/repos/yarn2-pnp-nonzero-install")
     app.deploy do |app|
-      expect(successful_body(app).strip).to eq("Hello from yarn-pnp-nonzero-install")
+      expect(successful_body(app).strip).to eq("Hello from yarn2-pnp-nonzero-install")
 
       run!('git commit --allow-empty -m "deploy again to test cache reuse"')
       app.push!
 
       expect(app.output).to include("Restoring cache")
       expect(app.output).to include("- yarn cache")
-      expect(successful_body(app).strip).to eq("Hello from yarn-pnp-nonzero-install")
+      expect(successful_body(app).strip).to eq("Hello from yarn2-pnp-nonzero-install")
+    end
+  end
+
+  it "node_modules nonzero install should deploy successfully and reuse the cache" do
+    app = Hatchet::Runner.new("spec/fixtures/repos/yarn2-modules-nonzero-install")
+    app.deploy do |app|
+      expect(successful_body(app).strip).to eq("Hello from yarn2-modules-nonzero-install")
+
+      run!('git commit --allow-empty -m "deploy again to test cache reuse"')
+      app.push!
+
+      expect(app.output).to include("Restoring cache")
+      expect(app.output).to include("- yarn cache")
+      expect(successful_body(app).strip).to eq("Hello from yarn2-modules-nonzero-install")
     end
   end
 end
