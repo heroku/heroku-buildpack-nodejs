a826e6dde7d9720ae7c4bc98847b97c82d447d02
Josh W Lewis <josh.lewis@salesforce.com>
2022-05-25 11:53:40 -0500

    Use PCRE regex on failure check

--- lib/failure.sh
+++ lib/failure.sh
@@ -421,7 +421,7 @@ fail_missing_yarn_vendor() {
 log_other_failures() {
   local log_file="$1"
 
-  if grep -Eq "version \`GLIBC_\d+.\d+' not found"; then
+  if grep -qP "version \`GLIBC_\d\+.\d+' not found"; then
     mcount "failures.libc6-incomapitility"
     meta_set "failure" "libc6-incomapitility"
     warn "This Node.js version is not compatible with the current stack.
