a639d0d18174b922cfe113a4b2901d33d9000b45
Josh W Lewis <josh.lewis@salesforce.com>
2022-10-03 15:33:45 -0500

    Don't try to detect CI branch name

--- etc/hatchet.sh
+++ etc/hatchet.sh
@@ -4,14 +4,11 @@ set -e
 
 ci_repo_owner=${CIRCLE_PROJECT_USERNAME:-${GITHUB_REPOSITORY_OWNER}}
 ci_repo_name=${CIRCLE_PROJECT_REPONAME:-${GITHUB_REPOSITORY}}
-ci_branch=${CIRCLE_BRANCH:-${GITHUB_REF_NAME}}
 
 [ "$ci_repo_owner" != "heroku" ] && echo "Run tests manually for forked PRs." && exit 0
 
 if [[ "$ci_repo_name" == *nodebin ]]; then
   HATCHET_BUILDPACK_BRANCH="main"
-elif [ -n "$ci_branch" ]; then
-  HATCHET_BUILDPACK_BRANCH="$ci_branch"
 else
   HATCHET_BUILDPACK_BRANCH=$(git name-rev HEAD 2> /dev/null | sed 's#HEAD\ \(.*\)#\1#' | sed 's#tags\/##')
 fi
