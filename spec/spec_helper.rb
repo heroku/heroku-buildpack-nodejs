acd445feaea624437e539288e7fa4f0421ca8ac1
Josh W Lewis <josh.lewis@salesforce.com>
2023-06-20 10:44:20 -0500

    Use the web_url to ensure we use the new domain names

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,7 +26,7 @@ end
 def successful_body(app, options = {})
   retry_limit = options[:retry_limit] || 100
   path = options[:path] ? "/#{options[:path]}" : ''
-  Excon.get("http://#{app.name}.herokuapp.com#{path}",
+  Excon.get("#{app.web_url}#{path}",
               idempotent:     true,
               expects:        200,
               retry_interval: 0.5,
