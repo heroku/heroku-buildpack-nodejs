7db2506d6f93c396d422d65a03a81cdd53d7e85d
Colin Casey <casey.colin@gmail.com>
2024-09-10 10:38:27 -0300

    Fixed root dir used for `COREPACK_HOME` in CI env

--- ci-profile/nodejs.sh
+++ ci-profile/nodejs.sh
@@ -3,5 +3,5 @@
 export PATH="$HOME/.heroku/node/bin:$HOME/.heroku/yarn/bin:$PATH:$HOME/bin:$HOME/node_modules/.bin"
 export NODE_HOME="$HOME/.heroku/node"
 export NODE_ENV=${NODE_ENV:-test}
-export COREPACK_HOME="$BUILD_DIR/.heroku/corepack"
+export COREPACK_HOME="$HOME/.heroku/corepack"
 export COREPACK_ENABLE_DOWNLOAD_PROMPT=0
