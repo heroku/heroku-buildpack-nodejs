3798578f892bfb023ae4e34f4de7fa29865382f6
Colin Casey <casey.colin@gmail.com>
2024-09-12 16:31:56 -0300

    Testing eaccess issue

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -334,7 +334,10 @@ pnpm_install() {
   fi
   save_pnpm_prune_store_counter "$cache_dir" "$(( counter - 1 ))"
 
+  md5=$(md5sum node_modules/.pnpm/@sentry+cli@1.77.3/node_modules/@sentry/cli/sentry-cli | cut -d ' ' -f 1)
+  echo "md5sum: $md5"
   ls -ld node_modules/.pnpm/@sentry+cli@1.77.3/node_modules/@sentry/cli/sentry-cli
+  find "$cache_dir" -type f -exec md5sum {} + | grep "$md5" | head -1 | cut -d ' ' -f 3 | xargs ls -ld
 }
 
 pnpm_prune_devdependencies() {
