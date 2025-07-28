f2e2a06a93750a1a4cb21fb9977b1afa7aab9159
Colin Casey <casey.colin@gmail.com>
2025-07-28 15:03:46 -0300

    Rename repo as heroku/nodejs-getting-started
    [W-18962249](https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07EE00002HNztCYAT/view)

--- spec/hatchet/ci_spec.rb
+++ spec/hatchet/ci_spec.rb
@@ -3,7 +3,7 @@ require_relative '../spec_helper'
 #Test CI deploys run tests and use the cache
 describe "Heroku CI" do
   it "Uses the cache" do
-    Hatchet::Runner.new("node-js-getting-started").run_ci do |test_run|
+    Hatchet::Runner.new("nodejs-getting-started").run_ci do |test_run|
       expect(test_run.output).to_not include("Restoring cache")
       test_run.run_again
       expect(test_run.output).to include("Restoring cache")
--- spec/hatchet/getting_started_spec.rb
+++ spec/hatchet/getting_started_spec.rb
@@ -3,7 +3,7 @@ require_relative '../spec_helper'
 #Test deploying the getting started guide works
 describe "Heroku node getting started" do
   it "clears runtime cache" do
-    Hatchet::Runner.new("node-js-getting-started").deploy do |app|
+    Hatchet::Runner.new("nodejs-getting-started").deploy do |app|
       #Deploy works
     end
   end
