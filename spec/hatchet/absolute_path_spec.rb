73545ae2aa4982838b814d1e01bc6f9c7dfb64c7
Colin Casey <casey.colin@gmail.com>
2025-07-29 16:34:12 -0300

    Rename repo as heroku/nodejs-getting-started (#1440)
    [W-18962249](https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07EE00002HNztCYAT/view)

--- spec/hatchet/absolute_path_spec.rb
+++ spec/hatchet/absolute_path_spec.rb
@@ -7,7 +7,7 @@ describe "Buildpack paths" do
       :default,
       "https://github.com/sharpstone/force_absolute_paths_buildpack"
     ]
-    Hatchet::Runner.new("node-js-getting-started", buildpacks: buildpacks).deploy do |app|
+    Hatchet::Runner.new("nodejs-getting-started", buildpacks: buildpacks).deploy do |app|
       #deploy works
     end
   end
