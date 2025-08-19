1a1ca9feae0b7e6bb859b112f56661979572b5c3
Colin Casey <casey.colin@gmail.com>
2025-08-19 14:31:39 -0300

    Remove `uses_workspaces`, `workspaces`, `num_project_files`, and `count_file_time` as these is not used in any reports

--- lib/builddata.sh
+++ lib/builddata.sh
@@ -21,21 +21,3 @@ log_initial_state() {
     fi
   done
 }
-
-# Log out information about the build that we can read from package.json
-log_project_info() {
-  local time
-  local build_dir="$1"
-
-  # Does this project use "workspaces"?
-  meta_set "uses_workspaces" "$(json_has_key "$build_dir/package.json" "workspaces")"
-  # What workspaces are defined? Logs as: `["packages/*","a","b"]`
-  meta_set "workspaces" "$(read_json "$build_dir/package.json" ".workspaces")"
-
-  # just to be sure this isn't disruptive, let's time it. This can be removed later once we've
-  # established that this is quick for all projects.
-  time=$(nowms)
-  # Count # of js, jsx, ts, coffee, vue, and html files to approximate project size, exclude any files in node_modules
-  meta_set "num_project_files" "$(find "$build_dir" -name '*.js' -o -name '*.ts' -o -name '*.jsx' -o -name '*.coffee' -o -name '*.vue' -o -name '*.html' | grep -cv node_modules | tr -d '[:space:]')"
-  meta_time "count_file_time" "$time"
-}
