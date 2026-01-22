a9c208b56ca26e8c45ea8e05dcf9b8e04c063c65
Colin Casey <casey.colin@gmail.com>
2026-01-22 11:11:07 -0400

    Add rebuild to check that cache restoration works with pnpm workspaces + pruning

--- spec/ci/pnpm_spec.rb
+++ spec/ci/pnpm_spec.rb
@@ -7,7 +7,19 @@ describe "pnpm support" do
       expect(app.output).to include("Using pnpm")
       expect(app.output).to include("Running 'pnpm install' with pnpm-lock.yaml")
       expect(app.output).to include("Pruning devDependencies")
-      expect(app.output).to include("devDependencies: skipped because NODE_ENV is set to production ")
+      expect(app.output).to include("devDependencies: skipped because NODE_ENV is set to production")
+      expect(successful_body(app).strip).to eq("Hello from pnpm workspace")
+
+      # Rebuild to test cache restoration and pruning still works
+      run!('git commit --allow-empty -m "rebuild to test caching"')
+      app.push!
+
+      # Second deploy should restore cache and prune again
+      expect(app.output).to include("Restoring cache")
+      expect(app.output).to include("- pnpm cache")
+      expect(app.output).to include("Running 'pnpm install' with pnpm-lock.yaml")
+      expect(app.output).to include("Pruning devDependencies")
+      expect(app.output).to include("devDependencies: skipped because NODE_ENV is set to production")
       expect(successful_body(app).strip).to eq("Hello from pnpm workspace")
     end
   end
