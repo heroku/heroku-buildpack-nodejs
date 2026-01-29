bc74f368a3650fde8f5d4a7b40332629c7d1dd68
Colin Casey <casey.colin@gmail.com>
2026-01-29 10:38:36 -0400

    Update hatchet test to get PATH info

--- spec/ci/corepack_spec.rb
+++ spec/ci/corepack_spec.rb
@@ -5,19 +5,19 @@ describe "corepack support" do
     app = Hatchet::Runner.new("spec/fixtures/repos/corepack-pnpm")
     app.deploy do |app|
       expect(successful_body(app).strip).to eq("Hello from corepack-pnpm")
+      app.run("pnpm --version").to match("???")
+      app.run("echo $PATH").to eq(<<~OUTPUT)
+        PATH=???
+      OUTPUT
     end
-    app.run("pnpm --version").to match("???")
-    app.run("echo $PATH").to eq(<<~OUTPUT)
-      PATH=???
-    OUTPUT
   end
 
-  it "should be able to run an app that installs yarn using corepack's packageManager field" do
-    app = Hatchet::Runner.new("spec/fixtures/repos/corepack-yarn")
-    app.deploy do |app|
-      expect(successful_body(app).strip).to eq("Hello from corepack-yarn")
-    end
-    app.run("yarn --version").to match("???")
-  end
+#   it "should be able to run an app that installs yarn using corepack's packageManager field" do
+#     app = Hatchet::Runner.new("spec/fixtures/repos/corepack-yarn")
+#     app.deploy do |app|
+#       expect(successful_body(app).strip).to eq("Hello from corepack-yarn")
+#     end
+#     app.run("yarn --version").to match("???")
+#   end
 end
 
