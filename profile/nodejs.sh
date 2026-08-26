db94384740c206f6bea8c34525dadbb59cd189aa
Colin Casey <casey.colin@gmail.com>
2026-08-26 13:06:12 -0300

    Migrate profile/ runtime scripts into the linting/formatting model (#1776)
    The profile/ scripts are sourced into the dyno boot shell at runtime rather
    than run during the build, so they are lint/format-only: tabs + shellcheck
    enable=all, but no namespace::function renaming and no strict-mode flags (set -e
    would kill the boot shell, set -u would break on unset env vars).
    
    Add profile/nodejs.sh and profile/WEB_CONCURRENCY.sh to MIGRATED_FILES and the
    .editorconfig migrated glob, and update the testProfileExport assertions to the
    braced (${HOME}/${PATH}) form now emitted by the migrated profile/nodejs.sh.

--- profile/nodejs.sh
+++ profile/nodejs.sh
@@ -1,28 +1,32 @@
-export PATH="$HOME/.heroku/node/bin:$HOME/.heroku/yarn/bin:$PATH:$HOME/bin:$HOME/node_modules/.bin"
-export NODE_HOME="$HOME/.heroku/node"
+#!/usr/bin/env bash
+
+export PATH="${HOME}/.heroku/node/bin:${HOME}/.heroku/yarn/bin:${PATH}:${HOME}/bin:${HOME}/node_modules/.bin"
+export NODE_HOME="${HOME}/.heroku/node"
 export NODE_ENV=${NODE_ENV:-production}
 
 # If the user has opted into the feature
-if [[ -n "$HEROKU_METRICS_URL" ]] && \
-   # if we're not on a one-off dyno
-   [[ "${DYNO}" != run\.* ]] && \
-   # the user has not opted out
-   [[ -z "$HEROKU_SKIP_NODE_PLUGIN" ]]; then
+if [[ -n "${HEROKU_METRICS_URL}" ]] \
+	&&
+	# if we're not on a one-off dyno
+	[[ "${DYNO:-}" != run\.* ]] \
+	&&
+	# the user has not opted out
+	[[ -z "${HEROKU_SKIP_NODE_PLUGIN}" ]]; then
 
-  if [[ -d $HOME/.heroku/heroku-nodejs-plugin ]]; then
-    # if the plugin was installed for this node version
-    metrics_handler="$HOME/.heroku/heroku-nodejs-plugin"
-  elif [[ -d $HOME/.heroku/metrics ]]; then
-    # or if the metrics script was installed
-    metrics_handler="$HOME/.heroku/metrics/metrics_collector.cjs"
-  else
-    return
-  fi
+	if [[ -d ${HOME}/.heroku/heroku-nodejs-plugin ]]; then
+		# if the plugin was installed for this node version
+		metrics_handler="${HOME}/.heroku/heroku-nodejs-plugin"
+	elif [[ -d ${HOME}/.heroku/metrics ]]; then
+		# or if the metrics script was installed
+		metrics_handler="${HOME}/.heroku/metrics/metrics_collector.cjs"
+	else
+		return
+	fi
 
-  # Don't clobber NODE_OPTIONS if the user has set it, just add the require flag to the end
-  if [[ -z "$NODE_OPTIONS" ]]; then
-    export NODE_OPTIONS="--require $metrics_handler"
-  else
-    export NODE_OPTIONS="${NODE_OPTIONS} --require $metrics_handler"
-  fi
+	# Don't clobber NODE_OPTIONS if the user has set it, just add the require flag to the end
+	if [[ -z "${NODE_OPTIONS}" ]]; then
+		export NODE_OPTIONS="--require ${metrics_handler}"
+	else
+		export NODE_OPTIONS="${NODE_OPTIONS} --require ${metrics_handler}"
+	fi
 fi
